29e8ed2180bdc314f545b07ac041ff5c4a36efa8
superkai-sdk1 <98566744+superkai-sdk1@users.noreply.github.com>
2025-02-12 23:55:34 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -803,31 +803,3 @@ modalBackground.addEventListener('click', () => {
     modal.classList.remove('show');
     modalBackground.classList.remove('show');
 });
-function collectGameResults() {
-    let results = [];
-    document.querySelectorAll('.main-game-table tbody tr').forEach(row => {
-        const number = row.querySelector('.col1').textContent.trim();
-        const nickname = row.querySelector('.nick_acpl').value.trim();
-        const role = row.querySelector('.role').textContent.trim();
-        const lh = row.querySelector('.lh-button')?.textContent.trim() || '';
-        const points = row.querySelector('.form-item .form-text').value.trim();
-        const addPoints = row.querySelector('.col7 .form-text').value.trim();
-        const total = row.querySelector('.col8').textContent.trim();
-
-        if (nickname) {
-            results.push({
-                number,
-                nickname,
-                role,
-                lh,
-                points,
-                addPoints,
-                total
-            });
-        }
-    });
-
-    const winner = document.querySelector('#winner_field').value === 'm' ? 'Победа Мафия' : 'Победа Мирные';
-    return { results, winner };
-}
-
